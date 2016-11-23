881d954f8e0a6d1a558bcecc66ebb8f3370012d0
Hp-pc <Hp-pc@192.168.2.13>
2016-11-23 10:44:46 +0530

    test commit

--- Santara2.0/src/com/fresh/santara/Santara.java
+++ Santara2.0/src/com/fresh/santara/Santara.java
@@ -1,7 +1,7 @@
 package com.fresh.santara;
 
 public class Santara {
-public static void main(String[] args) {
+public static <String> void main(String[] args) {
 	System.out.println("abc");
 }
 }
--- Santara2.0/src/com/fresh/santara/Test.java
+++ Santara2.0/src/com/fresh/santara/Test.java
@@ -1,1 +1,7 @@
-abc
+package com.fresh.santara;
+
+public class Test {
+public static <String> void main(String[] args) {
+	System.out.println("test");
+}
+}
