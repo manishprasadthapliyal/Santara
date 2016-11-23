106e76bc116fbcca06880647de5c56d7b75929a6
manishprasadthapliyal <manish54.thapliyal@gmail.com>
2016-11-23 15:27:38 +0530

    Update Santara.java
    test

--- Santara2.0/src/com/fresh/santara/Santara.java
+++ Santara2.0/src/com/fresh/santara/Santara.java
@@ -4,7 +4,7 @@ public class Santara {
 public static <String> void main(String[] args) {
 	System.out.println("abc");
 	System.out.println("push2");
-	System.out.println("pull2");
+	System.out.println("pull3");
 }
 }
 
