08cd2bfef090c19a5ccf136a16ea22be3e0f99b1
Hp-pc <Hp-pc@192.168.2.13>
2016-11-23 10:53:19 +0530

    test

--- Santara2.0/src/com/fresh/santara/Santara.java
+++ Santara2.0/src/com/fresh/santara/Santara.java
@@ -3,6 +3,6 @@ package com.fresh.santara;
 public class Santara {
 public static <String> void main(String[] args) {
 	System.out.println("abc");
-	System.out.println("push");
+	System.out.println("push2");
 }
 }
