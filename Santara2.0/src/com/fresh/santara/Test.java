62027d8ccdda778432867e8210ed5597c91fcc50
Hp-pc <Hp-pc@192.168.2.26>
2016-12-16 08:30:24 +0530

    commit push 5

--- Santara2.0/src/com/fresh/santara/Test.java
+++ Santara2.0/src/com/fresh/santara/Test.java
@@ -12,6 +12,7 @@ public static <String> void main(String[] args) {
 	System.out.println("push test3");
 	System.out.println("push test4");
 	System.out.println("pull test3");
+	System.out.println("push test5");
 	
 }
 }
